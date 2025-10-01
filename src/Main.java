ae4667abb62a7b680939441b87f1b92c9bba5a7a
eabr7 <e.bruins@protonmail.com>
2025-10-01 20:59:27 +0200

    Step 1 added to code. Made 3 arraylists of different type of products, containing 4 products each.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,45 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Product bread = new Product("Bread",4, 1.0);
-        Product cheese = new Product("Cheese", 4, 3.0);
-        Product fruit = new Product("Fruit", 4, 2.0);
-        Product toiletPaper = new Product("Toilet paper", 4, 2.5);
+
+        Product apple = new Product("Apple",10, 3.0);
+        Product banana = new Product("Banana", 5, 1.5);
+        Product pear = new Product("Pear", 8, 2.0);
+        Product peach = new Product("Peach", 5, 2.5);
+
+        ArrayList <Product> fruit = new ArrayList<>();
+        fruit.add(apple);
+        fruit.add(banana);
+        fruit.add(pear);
+        fruit.add(peach);
+
+        Product broccoli = new Product("Broccoli",6, 1.0);
+        Product carrot = new Product("Carrot", 20, 0.5);
+        Product zucchini = new Product("Zucchini", 10, 2.5);
+        Product spinach = new Product("Spinach", 15, 1.5);
+
+        ArrayList <Product> vegetables = new ArrayList<>();
+        fruit.add(broccoli);
+        fruit.add(carrot);
+        fruit.add(zucchini);
+        fruit.add(spinach);
+
+        Product milk = new Product("Milk",30, 1.0);
+        Product yoghurt = new Product("Yoghurt", 17, 2.0);
+        Product cheese = new Product("Cheese", 20, 3.5);
+        Product butter = new Product("Butter", 10, 1.5);
+
+        ArrayList <Product> dairy = new ArrayList<>();
+        fruit.add(milk);
+        fruit.add(yoghurt);
+        fruit.add(cheese);
+        fruit.add(butter);
+
+
+// ----------------------------------------------------------------------------------------------
+
         SuperMarket superMarket = new SuperMarket(bread, fruit, toiletPaper, cheese);
         Customer customer = new Customer("Customer");
 
